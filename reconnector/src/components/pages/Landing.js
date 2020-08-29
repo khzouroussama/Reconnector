6c7386b5770a28012ed6edfc43d5bf9f85efc189
Angelo Ibarrola <vino_1105@hotmail.com>
2020-08-29 10:42:27 -0700

    added landing page

---
+++ reconnector/src/components/pages/Landing.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+<header className="App-header">
+
+    <h1> Connector </h1>
+    <h4> Build your network and keep in touch!</h4>
+    
+    {/* BUTTON TO REGISTER */}
+</header>
