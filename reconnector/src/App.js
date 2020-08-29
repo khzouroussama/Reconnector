9857bf8620b83f5701e1e0a36fd0fd55c461c14b
Angelo Ibarrola <vino_1105@hotmail.com>
2020-08-29 10:33:08 -0700

    removed extras started landing page

--- reconnector/src/App.js
+++ reconnector/src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Connector </h1>
+        <h4> Build your network and keep in touch!</h4>
       </header>
     </div>
   );
