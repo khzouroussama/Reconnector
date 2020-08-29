ef21d27cb63934e5961f29789b304d79e3c0b3b8
Angelo Ibarrola <vino_1105@hotmail.com>
2020-08-29 15:21:28 -0700

    routed register button

--- reconnector/src/components/pages/Landing.js
+++ reconnector/src/components/pages/Landing.js
@@ -9,7 +9,7 @@ class Landing extends Component{
                     <div className="cyan darken-2 white-text">
                         <h1> Reconnector </h1>
                         <h5> Easily build your network and keep in touch! </h5>
-                        <a class="waves-effect waves-light btn-large"> Register </a>
+                        <a href="/register" class="waves-effect waves-light btn-large"> Register </a>
                     </div>
                 </div>
                 
