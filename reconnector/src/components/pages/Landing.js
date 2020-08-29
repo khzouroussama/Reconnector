f7c2c5b362acfb4cdfd33f0c0b8cca11b558875e
Angelo Ibarrola <vino_1105@hotmail.com>
2020-08-29 15:59:00 -0700

    centered landing page

--- reconnector/src/components/pages/Landing.js
+++ reconnector/src/components/pages/Landing.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react"
+import './Landing.css'
 
 
 class Landing extends Component{
     render(){
         return(
             <section>
-                <div className="container">
+                <div className="container Landing">
                     <div className="cyan darken-2 white-text">
                         <h1> Reconnector </h1>
                         <h5> Easily build your network and keep in touch! </h5>
-                        <a href="/register" class="waves-effect waves-light btn-large"> Register </a>
+                        <a href="/register" class="waves-effect waves-light red accent-2 btn-large"> Register </a>
                     </div>
                 </div>
                 
