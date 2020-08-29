ffd572b16c2f1e944953f76b3feed6f53a22abe9
Angelo Ibarrola <vino_1105@hotmail.com>
2020-08-29 13:58:53 -0700

    improve landing page

--- reconnector/src/components/pages/Landing.js
+++ reconnector/src/components/pages/Landing.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react"
 
+
 class Landing extends Component{
     render(){
         return(
             <section>
-                <div className="Landing">
-                    <h1> Reconnector </h1>
-                    <h3> Easily build your network and keep in touch! </h3>
+                <div className="container">
+                    <div className="cyan darken-2 white-text">
+                        <h1> Reconnector </h1>
+                        <h5> Easily build your network and keep in touch! </h5>
+                        <a class="waves-effect waves-light btn-large"> Register </a>
+                    </div>
                 </div>
+                
             </section>
         )
     }
