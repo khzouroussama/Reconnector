049992cf3cae00a41eb18d15e8ab734400c5783f
Angelo Ibarrola <vino_1105@hotmail.com>
2020-08-29 15:17:41 -0700

    added home and navbar

---
+++ reconnector/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React, {Componenet, Component} from 'react';
+
+
+class NavBar extends Component{
+    render(){
+        return(
+        <nav>
+            <div class="nav-wrapper">
+            <a href="/home" class="brand-logo">Reconnector</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html"><i class="large material-icons">exit_to_app</i></a></li>
+            </ul>
+            </div>
+        </nav>
+        )
+    }
+}
+
+export default NavBar
+
+
---
+++ reconnector/src/components/pages/Home.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+import Navbar from '../Navbar';
+
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                <Navbar />
+
+                <div className="container">
+                    <div className="cyan darken-2 white-text">
+
+                    </div>
+
+
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default Home
